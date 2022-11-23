0e061c7fe9b35c5a9819e5a4d1143e213cdaa0d4
jmechristian <jmechristian@gmail.com>
2022-11-23 17:46:09 -0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: 'Inter, sans-serif',
+        oswald: 'Oswald, san-serif',
+      },
+      colors: {
+        'ap-yellow': '#E4A800',
+        'ap-darkblue': '#005892',
+        'ap-blue': '#0873B8',
+        'ap-red': '#E43A00',
+      },
+    },
+  },
+  plugins: [],
+};
