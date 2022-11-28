f54b11c9f33a01a74eebe4cfcfb9e9a3a337a3ec
jmechristian <jmechristian@gmail.com>
2022-11-28 09:36:31 -0500

    stuff

--- features/layout/footer/FooterHeadline.js
+++ features/layout/footer/FooterHeadline.js
@@ -3,7 +3,7 @@ import TextWheel from '../../../shared/TextWheel';
 
 const FooterHeadline = () => {
   return (
-    <div className='flex flex-col gap-5'>
+    <div className='flex flex-col gap-5 md:gap-6'>
       <div className='flex justify-center'>
         <div className='flex flex-col md:flex-row-reverse md:items-end md:justify-center md:gap-0'>
           <div className='w-36 h-36 mx-auto relative left-10 md:left-0'>
@@ -13,7 +13,7 @@ const FooterHeadline = () => {
             <div className='uppercase font-bold text-lg md:text-xl text-white tracking-[.15em]'>
               Moments To
             </div>
-            <div className='text-ap-yellow font-medium font-oswald tracking-[.2em] uppercase text-4xl md:text-5xl ml-8'>
+            <div className='text-ap-yellow font-medium font-oswald tracking-[.2em] uppercase text-4xl md:text-5xl ml-8 md:ml-5'>
               Remember
             </div>
           </div>
