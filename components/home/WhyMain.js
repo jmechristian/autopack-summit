8fa9fef73d0f4912fb1ad11f8c89fd1f3af9654b
jmechristian <jmechristian@gmail.com>
2022-11-30 11:00:59 -0500

    why medium

--- components/home/WhyMain.js
+++ components/home/WhyMain.js
@@ -22,36 +22,39 @@ const WhyMain = () => {
 
   return (
     <div className='w-full h-full relative'>
-      <div className='py-12 px-8 max-w-7xl grid grid-cols-1 gap-y-12'>
-        <motion.div
-          className='flex flex-col items-center xl:gap-2 mx-auto'
-          ref={whyHeadlineRef}
-          variants={whyHeadlineVariants}
-          initial='hide'
-          animate={whyInView ? 'show' : 'hide'}
-        >
-          <motion.div className='blue_subheadline text-lg md:text-xl xl:text-2xl'>
-            More Than Just A
+      <div className='py-12 md:py-16 px-8 max-w-7xl grid grid-cols-1 md:grid-cols-2 md:gap-x-12 gap-y-12'>
+        <div className='flex flex-col gap-12 md:gap-32'>
+          <motion.div
+            className='flex flex-col items-center xl:gap-2 mx-auto md:mt-16'
+            ref={whyHeadlineRef}
+            variants={whyHeadlineVariants}
+            initial='hide'
+            animate={whyInView ? 'show' : 'hide'}
+          >
+            <motion.div className='blue_subheadline text-lg md:text-xl xl:text-2xl'>
+              More Than Just A
+            </motion.div>
+            <motion.div className='yellow_headline text-4xl md:text-5xl xl:text-6xl xl:text-center'>
+              Conference
+            </motion.div>
           </motion.div>
-          <motion.div className='yellow_headline text-4xl md:text-5xl xl:text-6xl xl:text-center'>
-            Conference
-          </motion.div>
-        </motion.div>
-        <div className='flex flex-col gap-12'>
           <OverlayWithText
             background='https://res.cloudinary.com/dno7xxmmy/image/upload/v1669772597/AutoPack%20Summit/IMG_2782_adqxqf.webp'
-            headline='Strategize'
+            headline='Collaborate'
             description='Learn cutting edge industry knowledge and trends by attending our educational sessions.'
             video='#'
             headlineColor='text-ap-blue'
           />
+        </div>
+        <div className='flex flex-col gap-12'>
           <OverlayWithText
             background='https://res.cloudinary.com/dno7xxmmy/image/upload/v1669772597/AutoPack%20Summit/IMG_2874_iijxzx.webp'
-            headline='Collaborate'
+            headline='Strategize'
             description='Learn cutting edge industry knowledge and trends by attending our educational sessions.'
             video='#'
             headlineColor='text-ap-darkblue'
           />
+
           <OverlayWithText
             background='https://res.cloudinary.com/dno7xxmmy/image/upload/v1669772597/AutoPack%20Summit/IMG_2022_hbtevz.webp'
             headline='Innovate'
