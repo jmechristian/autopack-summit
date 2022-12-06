2fb24468a236545714e420881390d797aa14a83d
jmechristian <jmechristian@gmail.com>
2022-12-06 13:49:54 -0500

    agenda

--- components/agenda/AgendaHead.js
+++ components/agenda/AgendaHead.js
@@ -2,18 +2,12 @@ import React from 'react';
 
 const AgendaHead = () => {
   return (
-    <div className='bg-bgImage_blue pt-12 bg-cover bg-center'>
-      <div className='flex flex-col gap-8 justify-center items-center mx-auto max-w-7xl py-12 md:py-20 lg:py-24 px-8 lg:px-16'>
-        <div className='flex flex-col gap-3'>
-          <div className='white_headline text-[2.7rem] md:text-7xl lg:text-8xl xl:text-9xl leading-none text-center'>
-            Agenda
-          </div>
-          <div className='text-slate-300 text-center'>
-            Experience the premier open forum for OEMs to discuss their
-            packaging innovations and challenges.{' '}
-          </div>
+    <div className='bg-bgImage_blue pt-16 lg:pt-20 bg-cover bg-center'>
+      <div className='flex flex-col gap-8 justify-center items-center mx-auto max-w-7xl py-12 lg:py-16 px-8 lg:px-16'>
+        <div className='white_headline text-[2.7rem] md:text-7xl lg:text-7xl xl:text-8xl leading-none text-center'>
+          Agenda
         </div>
-        <div className='grid grid-cols-3'>
+        <div className='grid grid-cols-3 md:mt-6'>
           <div className='border-b-4 border-b-ap-yellow pb-2'>
             <div className='font-medium text-xl uppercase font-oswald text-white tracking-widest px-4'>
               Day 1
@@ -31,10 +25,10 @@ const AgendaHead = () => {
           </div>
         </div>
         <div className='flex flex-col text-center'>
-          <div className='uppercase tracking-widest text-sm text-white font-bold'>
+          <div className='uppercase tracking-widest text-sm lg:text-base text-white font-bold'>
             Oct 12, 2023
           </div>
-          <div className='uppercase font-medium font-oswald tracking-[.2em] text-3xl text-ap-yellow'>
+          <div className='uppercase font-medium font-oswald tracking-[.2em] text-2xl lg:text-3xl text-ap-yellow'>
             Wednesday
           </div>
         </div>
--- components/agenda/AgendaItem.js
+++ components/agenda/AgendaItem.js
@@ -3,27 +3,47 @@ import Image from 'next/image';
 
 const AgendaItem = () => {
   return (
-    <div className='flex flex-col gap-8 border-t border-t-slate-800 py-9'>
-      <div className='flex flex-col gap-1'>
-        <div className='text-slate-500'>9:00 AM - 10:00 AM</div>
-        <div className='font-bold text-2xl'>
-          Optional Tour at PackIQ Container Management Center
+    <div className='grid grid-cols-1 md:grid-cols-5 gap-8 border-t border-t-slate-800 py-9 lg:py-12 md:px-4'>
+      <div className='flex flex-col lg:grid lg:grid-cols-5 gap-1 lg:gap-8 md:col-span-3 lg:col-span-4'>
+        <div className='flex flex-col gap-6'>
+          <div className='text-slate-400 hidden lg:inline-block uppercase tracking-widest font-bold text-sm'>
+            time
+          </div>
+          <div className='text-slate-500 lg:col-span-1'>9:00AM - 10:00AM</div>
         </div>
-        <div className='flex flex-col mt-3'>
-          <div className='font-bold'>Kellen Mahoney</div>
-          <div className='text-slate-500'>
-            Suppliers Partnership for the Environment
+        <div className='flex flex-col gap-6  lg:col-span-2'>
+          <div className='text-slate-400 hidden lg:inline-block uppercase tracking-widest font-bold text-sm'>
+            session
+          </div>
+          <div className='font-bold text-xl'>
+            Optional Tour at PackIQ Container Management Center
+          </div>
+        </div>
+        <div className='flex flex-col gap-6 lg:col-span-2'>
+          <div className='text-slate-400 uppercase tracking-widest font-bold text-sm hidden lg:inline-block'>
+            speaker
+          </div>
+          <div className='flex flex-col mt-3 lg:mt-0 lg:col-span-2'>
+            <div className='font-bold'>Kellen Mahoney</div>
+            <div className='text-slate-500'>
+              Suppliers Partnership for the Environment
+            </div>
           </div>
         </div>
       </div>
-      <div className='w-1/2'>
-        <div className='p-2'>
-          <Image
-            src='https://res.cloudinary.com/dno7xxmmy/image/upload/v1670254640/AutoPack%20Summit/packiq_ejthac.png'
-            width='500'
-            height='164'
-            alt='Pack IQ'
-          />
+      <div className=' md:col-span-2 lg:col-span-1 flex justify-end items-center lg:items-start'>
+        <div className='flex flex-col gap-6 lg:col-span-2'>
+          <div className='text-slate-400 uppercase tracking-widest font-bold text-sm hidden lg:inline-block'>
+            speaker
+          </div>
+          <div className='p-2 w-1/2 md:w-2/3 lg:w-full'>
+            <Image
+              src='https://res.cloudinary.com/dno7xxmmy/image/upload/v1670254640/AutoPack%20Summit/packiq_ejthac.png'
+              width='500'
+              height='164'
+              alt='Pack IQ'
+            />
+          </div>
         </div>
       </div>
     </div>
