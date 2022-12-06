534d7a5df609042d48ba430fd612d83aaf11d404
jmechristian <jmechristian@gmail.com>
2022-12-06 18:00:13 -0500

    distinguish

--- components/sponsorship/SponsorshipHead.js
+++ components/sponsorship/SponsorshipHead.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {
+  TrophyIcon,
+  SparklesIcon,
+  ArrowLongRightIcon,
+} from '@heroicons/react/24/solid';
 
 const SponsorshipHead = () => {
   return (
-    <div className='bg-bgImage_blue pt-16 md:pt-24 lg:pt-24 xl:pt-28 bg-cover bg-center relative'>
+    <div className='bg-bgImage_blue pt-16 lg:pt-24 xl:pt-28 bg-cover bg-center relative'>
       <div className='flex flex-col gap-5 justify-center items-center mx-auto max-w-7xl py-12 md:py-16 px-8 lg:px-16'>
         <div className='flex flex-col gap-2'>
           <div className='white_headline text-[2.7rem] md:text-7xl lg:text-7xl xl:text-8xl leading-none text-center'>
@@ -12,12 +17,18 @@ const SponsorshipHead = () => {
             Your Business
           </div>
         </div>
-        <div className='text-slate-300 text-center mb-20 md:mb-24 lg:mb-28 xl:mb-52 max-w-prose md:px-12 xl:text-lg'>
+        <div className='text-slate-300 text-center mb-20 md:mb-28 lg:mb-44 xl:mb-56 max-w-prose md:px-12 xl:text-lg'>
           Experience the premier open forum for OEMs to discuss their packaging
           innovations and challenges.
         </div>
       </div>
-      <div className='bg-slate-200 aspect-video w-11/12 md:w-7/12 lg:w-1/2 absolute shadow-lg -bottom-28 md:-bottom-40 lg:-bottom-44 xl:-bottom-64 2xl:-bottom-72 right-0 z-10'></div>
+      <div className='bg-slate-200 aspect-video w-11/12 md:w-9/12 lg:w-7/12 xl:w-1/2 absolute shadow-lg -bottom-24 md:-bottom-52 lg:-bottom-52 xl:-bottom-64 2xl:-bottom-72 right-0 z-10'></div>
+      <div className='absolute bottom-12 mx-auto max-w-7xl left-0 hidden lg:inline-block z-30 bg-white/20 py-5 px-3 backdrop-blur-md drop-shadow-xl rounded-r-lg'>
+        <div className='flex gap-2 w-full justify-center px-3'>
+          <SparklesIcon className='w-7 h-7 fill-white' />
+          <div className='font-medium text-white'>Join The Sponsor Lineup</div>
+        </div>
+      </div>
     </div>
   );
 };
