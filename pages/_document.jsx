4320656d722b405ba5abe86c56c8374227aa9c89
jmechristian <jmechristian@gmail.com>
2023-01-06 13:32:37 -0500

    GA

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -13,6 +14,24 @@ export default function Document() {
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Oswald:wght@200;400;500;700&display=swap'
           rel='stylesheet'
+        />
+        <Script
+          strategy='afterInteractive'
+          src='https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX'
+        />
+            
+        <Script
+          id='google-analytics'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7TRT33B5PN');
+        `,
+          }}
         />
       </Head>
       <body>
