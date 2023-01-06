286b748727c90ac6c9c13782bc937b22e96f158d
jmechristian <jmechristian@gmail.com>
2023-01-06 13:43:53 -0500

    ddddd

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -19,34 +19,29 @@ export default function Document() {
           strategy='afterInteractive'
           src='https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX'
         />
-            
         <Script
           id='google-analytics'
           strategy='afterInteractive'
           dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
+            __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-7TRT33B5PN');
-        `,
+  gtag('config', 'G-7TRT33B5PN');`,
           }}
         />
         <Script
           id='hotjar'
           strategy='afterInteractive'
           dangerouslySetInnerHTML={{
-            __html: `
-                      (function(h,o,t,j,a,r){
+            __html: `(function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
               h._hjSettings={hjid:3312719,hjsv:6};
               a=o.getElementsByTagName('head')[0];
               r=o.createElement('script');r.async=1;
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
               a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `,
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
           }}
         />
       </Head>
