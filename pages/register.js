f6bd6e95c75bfc4adc8fcb81f5d1b450e2a8ad2e
jmechristian <jmechristian@gmail.com>
2023-01-10 16:25:18 -0500

    xxxxxx

--- pages/register.js
+++ pages/register.js
@@ -6,7 +6,7 @@ const register = () => {
   return (
     <div className='flex flex-col'>
       <div className='h-16 md:h-20 lg:h-24 bg-ap-darkblue'></div>
-      <div className='py-12 md:py-24 lg:py-20 xl:py-20 flex flex-col gap-8 xl:gap-12'>
+      <div className='py-12 md:py-24 lg:py-12 flex flex-col gap-8 xl:gap-12'>
         <div className='flex flex-col items-center'>
           <motion.div className='blue_subheadline text-xl md:text-xl xl:text-2xl'>
             OEM/ TIER 1
