9fa62ced41060327c1d95f0c18e679fb3828b0c4
jmechristian <jmechristian@gmail.com>
2023-01-17 19:25:08 -0500

    denied

--- pages/denyregistration.js
+++ pages/denyregistration.js
@@ -2,16 +2,26 @@ import React from 'react';
 
 const denyRegistration = () => {
   return (
-    <div className='w-screen h-screen bg-ap-darkblue flex justify-center'>
-      <div className='pt-12 w-5xl'>
-        <iframe
-          src='https://giphy.com/embed/l3V0px8dfZmmfwize'
-          width='600px'
-          height='100%'
-          frameBorder='0'
-          class='giphy-embed'
-          allowFullScreen
-        ></iframe>
+    <div className='w-screen h-screen bg-ap-darkblue flex justify-center items-center'>
+      <div className='pt-12'>
+        <div
+          style={{
+            width: '600px',
+            height: 0,
+            paddingBottom: '56%',
+            position: 'relative',
+          }}
+        >
+          <iframe
+            src='https://giphy.com/embed/l3V0px8dfZmmfwize'
+            width='100%'
+            height='100%'
+            style={{ position: 'absolute' }}
+            frameBorder='0'
+            class='giphy-embed'
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </div>
   );
