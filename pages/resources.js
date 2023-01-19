036966bbedc5d6906d54b2915756665199f7e2ed
jmechristian <jmechristian@gmail.com>
2023-01-19 17:44:38 -0500

    hry

--- pages/resources.js
+++ pages/resources.js
@@ -3,11 +3,12 @@ import { createClient } from 'next-sanity';
 import { motion } from 'framer-motion';
 import ResourceGrid from '../components/resources/ResourceGrid';
 import AdvisoryBoard from '../components/resources/AdvisoryBoard';
+import HeaderPadding from '../shared/HeaderPadding';
 
 const resources = ({ resourceData }) => {
   return (
     <div className='flex flex-col'>
-      <div className='h-16 md:h-20 lg:h-24 bg-ap-darkblue'></div>
+      <HeaderPadding />
       <div className='pt-12 md:pt-16 lg:pt-20 xl:pt-20 xl:pb-12 flex flex-col gap-8 md:gap-12 xl:gap-12'>
         <div className='flex flex-col items-center'>
           <motion.div className='blue_subheadline text-xl md:text-xl xl:text-2xl'>
---
+++ shared/HeaderPadding.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HeaderPadding = () => {
+  return <div className='h-16 md:h-20 lg:h-24 bg-ap-darkblue'></div>;
+};
+
+export default HeaderPadding;
