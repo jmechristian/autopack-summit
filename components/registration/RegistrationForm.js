0d00391da55518f31d7ffa1ead546c5f85e31c7e
jmechristian <jmechristian@gmail.com>
2023-01-18 20:48:50 -0500

    main reg done

--- components/registration/RegistrationForm.js
+++ components/registration/RegistrationForm.js
@@ -38,11 +38,11 @@ const RegistrationForm = () => {
         <RegistrationFormMobile codes={codes && codes} />
       </div> */}
       {submitted ? (
-        <div className='max-w-xl lg:max-w-2xl text-center px-6 md:px-10 my-24 mx-auto'>
+        <div className='max-w-xl lg:max-w-2xl text-center px-10 my-24 mx-auto'>
           <h2 className='text-3xl font-bold tracking-tight text-ap-darkblue sm:text-5xl lg:text-5xl'>
             Thank You!
           </h2>
-          <p className='mt-5 md:text-lg leading-loose lg:text-xl text-gray-500'>
+          <p className='mt-5 md:text-lg leading-relaxed lg:leading-relaxed lg:text-xl text-gray-500'>
             Your submission has been sent. We look forward to collaborating with
             you in Greenville. For any questions, please email{' '}
             <a href='mailto:diana@packagingschool.com' className='font-bold'>
