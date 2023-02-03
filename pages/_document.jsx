3c9a523ad6f6cef470b9f02f3d57f4faf611bffd
jmechristian <jmechristian@gmail.com>
2023-02-03 11:26:01 -0500

    ssss

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html style={{ scrollBehavior: 'smooth' }}>
       <Head>
+        <title>Automotive Packaging Summit</title>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
