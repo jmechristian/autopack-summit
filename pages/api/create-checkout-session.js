e88848810c1fa58ccb466e5bcfe95379cc53cade
jmechristian <jmechristian@gmail.com>
2023-08-24 16:20:07 -0400

    ssss

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -25,27 +25,27 @@ export default async function handler(req, res) {
     req.body.phone
   );
 
-  // if (req.method === 'POST') {
-  try {
-    // Create Checkout Sessions from body params.
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: 'price_1Nhx8iBmL7ZGJcSLDlz1RgHC',
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: `${req.headers.origin}/registration-confirmation?success=true`,
-      cancel_url: `${req.headers.origin}/registration-confirmation?canceled=true`,
-    });
-    res.redirect(302, session.url);
-  } catch (err) {
-    res.status(err.statusCode || 500).json(err.message);
+  if (req.method === 'POST') {
+    try {
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: 'price_1Nhx8iBmL7ZGJcSLDlz1RgHC',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/registration-confirmation?success=true`,
+        cancel_url: `${req.headers.origin}/registration-confirmation?canceled=true`,
+      });
+      res.redirect(302, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
-  // } else {
-  //   res.setHeader('Allow', 'POST');
-  //   res.status(405).end('Method Not Allowed');
-  // }
 }
