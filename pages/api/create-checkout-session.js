29617794185f3dd06f822a30a3a254d08cf6ca7e
jmechristian <jmechristian@gmail.com>
2023-08-24 16:12:00 -0400

    ssss

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
         success_url: `${req.headers.origin}/registration-confirmation?success=true`,
         cancel_url: `${req.headers.origin}/registration-confirmation?canceled=true`,
       });
-      res.redirect(303, session.url);
+      res.redirect(302, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
