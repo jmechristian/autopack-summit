8f67fc6380d2721913cf9afb6fb0ab4f2f0a5d24
jmechristian <jmechristian@gmail.com>
2023-09-02 13:41:47 -0400

    sssss

--- components/registerProvider/RegistrationTickets.js
+++ components/registerProvider/RegistrationTickets.js
@@ -132,7 +132,15 @@ const RegistrationTickets = ({ ticketCount }) => {
                 className='bg-ap-yellow rounded-md w-full'
                 type='submit'
                 role='link'
-                disabled={20 - ticketCount === 0 ? true : false}
+                disabled={
+                  !name ||
+                  !title ||
+                  !email ||
+                  !phone ||
+                  !title ||
+                  !company ||
+                  20 - ticketCount === 0
+                }
               >
                 <div className='text-slate-800 font-oswald uppercase text-sm lg:text-lg font-bold py-3 px-6 tracking-widest lg:leading-tight'>
                   {isLoading ? 'Sending Data' : 'Proceed to Payment'}
