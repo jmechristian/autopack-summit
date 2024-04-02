3f5c990dd556fc2150410929d5a9a4e46511f760
jmechristian <jmechristian@gmail.com>
2024-04-02 09:18:30 -0400

    ssss

--- pages/api/create-early-bird-checkout.js
+++ pages/api/create-early-bird-checkout.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: 'price_1OtHKfBmL7ZGJcSLmYirq6ve',
+            price: 'price_1P0olbBmL7ZGJcSL4BF5frQt',
             quantity: 1,
             // email: req.body.email,
           },
