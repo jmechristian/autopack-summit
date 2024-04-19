55450e04e7bab475cbd20e277d0f83b52281e97a
jmechristian <jmechristian@gmail.com>
2024-04-19 13:44:18 -0400

    surg

--- pages/api/send-surgere-approval.js
+++ pages/api/send-surgere-approval.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
         },
         Subject: {
           Charset: 'UTF-8',
-          Data: 'Surgere Tour Confirmation',
+          Data: 'Surgere Workshop Confirmation',
         },
       },
       Source: fromAddress,
--- pages/api/send-surgere.js
+++ pages/api/send-surgere.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
         },
         Subject: {
           Charset: 'UTF-8',
-          Data: 'Surgere Tour Submission Form',
+          Data: 'Surgere Workshop Submission Form',
         },
       },
       Source: fromAddress,
