18b757b4fe96eb42998db8bd28d1f1207bfb913c
jmechristian <jmechristian@gmail.com>
2025-04-14 08:16:37 -0400

    sdsdsd

--- pages/api/send-registration-confirmation.js
+++ pages/api/send-registration-confirmation.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
         ToAddresses: [
           toAddress,
           //   body.email,
-          'lars@packagingschool.com',
-          'bianca@packagingschool.com',
+          // 'lars@packagingschool.com',
+          // 'bianca@packagingschool.com',
         ],
       },
       Message: {
